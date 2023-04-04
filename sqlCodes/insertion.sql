2998beb1cf356de45cdb9f0e4c3cd757ced8b977
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-04-04 15:12:26 +0530

    Add old files

--- sqlCodes/insertion.sql
+++ sqlCodes/insertion.sql
@@ -1,1 +1,29 @@
 insert into cities (name) values  ("Bangalore"), ("Delhi") , ("Guwahati") ;
+
+insert into workshops (name, total_slots, city_id) values ("BLR-Workshop1", 4, 1); 
+
+insert into workshops (name, total_slots, city_id) values ("BLR-Workshop2", 3, 1); 
+
+insert into workshops (name, total_slots, city_id) values ("GHY-Workshop1", 2, 3); 
+
+insert into workshops (name, total_slots, city_id) values ("DLH-Workshop1", 7, 2); 
+
+insert into workshops (name, total_slots, city_id) values ("BLR-Workshop3", 3, 1); 
+
+
+
+
+
+insert into users (name, phone, mail) values ("Anurag", "1234", "abcd@gmail.com");
+
+insert into users (name, phone, mail) values ("Zubayer", "2234", "bcd@gmail.com");
+
+insert into users (name, phone, mail) values ("John", "3234", "dcd@gmail.com");
+
+insert into users (name, phone, mail) values ("Harry", "6234", "dde@gmail.com");
+
+insert into users (name, phone, mail) values ("Ron", "23234", "cde@gmail.com");
+
+insert into users (name, phone, mail) values ("Jack", "73234", "fde@gmail.com");
+
+insert into users (name, phone, mail) values ("Jake", "93234", "hde@gmail.com");
---
+++ sqlCodes/storedProcedure.sql
@@ -0,0 +1,40 @@
+-- Insertion into slots_availability
+INSERT INTO slots_availability (workshop_id, date, available_slots) 
+SELECT id, CURDATE() + INTERVAL 1 DAY, total_slots
+FROM workshops;
+
+-- Afterwards,
+-- Updating slots_availability regularly to reset available_slots, and change date to be the next day
+DELIMITER $$
+CREATE PROCEDURE IF NOT EXISTS updateSlots()
+BEGIN
+UPDATE slots_availability
+SET
+    date = CURDATE() + INTERVAL 1 DAY,
+    available_slots = (SELECT total_slots FROM workshops WHERE id = workshop_id);
+END $$
+DELIMITER ;
+
+
+
+-- Stored Procedure for booking: [without locking]
+-- With locking in "lockin1.sql"
+DELIMITER $$
+CREATE PROCEDURE IF NOT EXISTS book(IN wid INT, IN uid INT, IN bdate DATE)
+BEGIN
+    if(0 < ( select available_slots from slots_availability where workshop_id = wid and date = bdate)) then 
+        update slots_availability
+        set
+        available_slots = available_slots-1
+        where
+        workshop_id = wid;
+        insert into bookings(workshop_id, user_id, booking_date, date_created) values(wid, uid, bdate, now());
+       
+    else
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'There are no slots available for booking on the given date at the specified workshop.';
+    end if;
+    select * from bookings;
+    select * from slots_availability;
+END $$
+DELIMITER ;
