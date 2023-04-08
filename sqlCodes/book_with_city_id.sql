92bdfe1b7d820984116b4ebd222c2ba24d853464
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-04-08 21:42:09 +0530

    Add code

--- sqlCodes/book_with_city_id.sql
+++ sqlCodes/book_with_city_id.sql
@@ -2,8 +2,10 @@ DELIMITER $$
 CREATE PROCEDURE IF NOT EXISTS book_with_city_id(IN cid INT, IN uid INT, IN bdate DATE)
 BEGIN
     -- Declarations
-    DECLARE workshop_ids INT;
+    DECLARE wid INT;
     DECLARE available_slots INT;
+    DECLARE num_zeros INT;
+    DECLARE total_rows INT;
 
 
     -- DECLARE workshopsInCity IS VARRAY() OF INTEGER;
@@ -14,34 +16,33 @@ BEGIN
     -- START TRANSACTION;
 
     -- Find workshops in a city
+    DROP table same_city_workshops;
     CREATE TEMPORARY TABLE same_city_workshops SELECT * FROM slots_availability where workshop_id in (
         SELECT id from workshops where city_id = cid
     );
 
+    select count(*) into num_zeros from same_city_workshops where available_slots = 0;
+    select count(*) into total_rows from same_city_workshops;
 
-    
 
-    -- for workshop in workshops_ids 
-        select available_slots into num_slots
-        from slots_availability
-        where workshop_id = wid and date = bdate
-        for update;
-    
-        -- if slots are available, then book   
-        if(num_slots > 0) then 
-            update slots_availability
-            set available_slots = available_slots - 1
-            where workshop_id = wid and date = bdate;
-
-            -- Commit the changes to the database, release the exclusive lock on slots_availability
-            COMMIT;
-
-            -- Insert into bookings table doesn't require exclusive lock
-            insert into bookings(workshop_id, user_id, booking_date, date_created) values(wid, uid, bdate, now());
+    if (num_zeros != total_rows) then
+        select workshop_id into wid from same_city_workshops where date = bdate limit 0, 1;
+        -- select workshop_id from same_city_workshops where date = bdate limit 0, 1;
         
-        else
-            --  move to next wid  
-        end if;
+        
+        -- update slots_availability
+        -- set available_slots = available_slots-1
+        -- where workshop_id = wid and date = bdate;
+
+        select * from slots_availability
+        where workshop_id = wid and date = bdate;
+
+
+        -- insert into bookings(workshop_id, user_id, booking_date, date_created) values(wid, uid, bdate, now());
+
+    end if;
+
+    
 
     -- Find slots in one of the workshops
     
@@ -52,3 +53,50 @@ BEGIN
     
 END $$
 DELIMITER ;
+
+
+-- DECLARE @MyCursor CURSOR;
+    -- BEGIN
+    --     SET @MyCursor = CURSOR FOR same_city_workshops.workshop_id
+
+    --     OPEN @MyCursor 
+    --     FETCH NEXT FROM @MyCursor 
+    --     INTO wid
+
+    --     WHILE @@FETCH_STATUS = 0
+    --     BEGIN
+    --     /*
+    --         YOUR ALGORITHM GOES HERE   
+    --     */
+    --     FETCH NEXT FROM @MyCursor 
+    --     INTO @MyField 
+    --     END; 
+
+    --     CLOSE @MyCursor ;
+    --     DEALLOCATE @MyCursor;
+    -- END;
+
+
+    
+
+    -- -- for workshop in workshops_ids 
+    --     select available_slots into num_slots
+    --     from slots_availability
+    --     where workshop_id = wid and date = bdate
+    --     for update;
+    
+    --     -- if slots are available, then book   
+    --     if(num_slots > 0) then 
+    --         update slots_availability
+    --         set available_slots = available_slots - 1
+    --         where workshop_id = wid and date = bdate;
+
+    --         -- Commit the changes to the database, release the exclusive lock on slots_availability
+    --         COMMIT;
+
+    --         -- Insert into bookings table doesn't require exclusive lock
+    --         insert into bookings(workshop_id, user_id, booking_date, date_created) values(wid, uid, bdate, now());
+        
+    --     else
+    --         --  move to next wid  
+    --     end if;
