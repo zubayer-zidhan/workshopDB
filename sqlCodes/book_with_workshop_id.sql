3e2b6ecb3f07f93e34c0e25edfd1c5b6c98f957a
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-04-18 22:52:23 +0530

    Updated book_with_workshop_id

--- sqlCodes/book_with_workshop_id.sql
+++ sqlCodes/book_with_workshop_id.sql
@@ -1,9 +1,8 @@
 DELIMITER $$
 CREATE PROCEDURE IF NOT EXISTS book_with_workshop_id(IN wid INT, IN uid INT, IN bdate DATE)
 BEGIN
-    -- Declarations
+        -- Declarations
     DECLARE num_slots INT;
-    DECLARE isLocked INT;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION ROLLBACK;
 
     -- Starting the transaction
@@ -12,11 +11,10 @@ BEGIN
     -- Selecting the number of available slots in a given workshop
     -- "for update" grants exclusive lock to the slots-availability table
       -- TODO: Need to lock the temp table ???
-    SELECT GET_LOCK("workshops_lock", 5) INTO isLocked;
-
     select available_slots into num_slots
     from slots_availability
-    where workshop_id = wid and date = bdate;
+    where workshop_id = wid and date = bdate
+    for update;
     
 
     -- if slots are available, then book   
@@ -28,7 +26,6 @@ BEGIN
         -- TODO: Release locks
         -- 10 = SUCCESS
         SELECT 10; 
-        SELECT RELEASE_LOCK("workshops_lock");
 
         -- Commit the changes to the database, release the exclusive lock on slots_availability
         COMMIT;
@@ -36,12 +33,10 @@ BEGIN
         -- Insert into bookings table doesn't require exclusive lock
         INSERT INTO bookings(workshop_id, user_id, booking_date, date_created) values(wid, uid, bdate, now());
        
-    -- TODO: If no slots, then ROLLBACK
     ELSEIF (num_slots = 0) THEN
         -- Error encountered, rollback, release locks
         -- 20 = Slots Full
         SELECT 20; 
-        SELECT RELEASE_LOCK("workshops_lock");
         -- Error encountered, rollback, release locks
         ROLLBACK;
         -- SIGNAL SQLSTATE '45000'
@@ -49,66 +44,10 @@ BEGIN
     ELSE
 		-- 30 = SOME OTHER ERROR
 		SELECT 30;
-		SELECT RELEASE_LOCK("workshops_lock");
         ROLLBACK;
 
     end if;
 
-    -- select * from bookings;
-    -- select * from slots_availability;
+
 END $$
 DELIMITER ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- ************************ LOCKING IS NOT ALLOWED IN STORED PROCEDURE *************************
--- DELIMITER $$
--- CREATE PROCEDURE IF NOT EXISTS book1(IN wid INT, IN uid INT, IN bdate DATE)
--- BEGIN
---     DECLARE EXIT HANDLER FOR SQLEXCEPTION ROLLBACK;
-
---     START TRANSACTION;
-
---     LOCK TABLES slots_availability WRITE;
-   
---     if(0 < (select available_slots from slots_availability where workshop_id = wid and date = bdate) ) then 
---         update slots_availability
---         set
---         available_slots = available_slots-1
---         where
---         workshop_id = wid;
-
---         UNLOCK TABLES;
-
---         insert into bookings(workshop_id, user_id, booking_date, date_created) values(wid, uid, bdate, now());
-       
---     else
---         SIGNAL SQLSTATE '45000'
---         SET MESSAGE_TEXT = 'There are no slots available for booking on the given date at the specified workshop.';
---     end if;
-
---     UNLOCK TABLES;
-
---     select * from bookings;
---     select * from slots_availability;
--- END $$
--- DELIMITER ;
