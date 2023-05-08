cd4ad4f52736cea4b98d38cf3875b911a7755f55
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-05-08 16:19:33 +0530

    updates

--- sqlCodes/available_slots_in_workshop.sql
+++ sqlCodes/available_slots_in_workshop.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `available_slots_in_workshop`(IN wid int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `available_slots_in_workshop`(IN wid int, IN bdate DATE)
 BEGIN
-	SELECT available_slots from slots_availability where workshop_id = wid;
+	SELECT available_slots from slots_availability where workshop_id = wid AND date = bdate;
 END
